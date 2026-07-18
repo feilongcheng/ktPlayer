af489eac1f525ffa471c4b16618a2cb587734e8e
Felix <me@chengfeilong.com>
2026-07-18 20:24:24 +1200

    Fix transform

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,32 +1,42 @@
-module.exports = function(grunt){
+const sass = require('sass');
+
+module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-        uglify:{
-            options:{
-                stripBanners:true,
-                banner:'/* ktPlayer by Scott */\n'
+
+        uglify: {
+            options: {
+                stripBanners: true,
+                banner: '/* ktPlayer by Felix */\n'
             },
-            build:{
-                src:'./src/ktPlayer.js',
-                dest:'./dist/ktPlayer.min.js'
+            build: {
+                src: './src/ktPlayer.js',
+                dest: './dist/ktPlayer.min.js'
             }
         },
-        sass:{
-            dist:{
-                options:{
-                    style:'compressed'
+
+        sass: {
+            options: {
+                implementation: sass,
+                sourceMap: false
+            },
+            dist: {
+                options: {
+                    style: 'compressed'
                 },
-                files:[{
-                    expand:true,
-                    cwd:'./src',
-                    src:['*.scss'],
-                    dest:'./dist',
-                    ext:'.min.css'
+                files: [{
+                    expand: true,
+                    cwd: './src',
+                    src: ['*.scss'],
+                    dest: './dist',
+                    ext: '.min.css'
                 }]
             }
         }
     });
+
     grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.registerTask('default',['uglify','sass']);
+    grunt.loadNpmTasks('grunt-sass');
+
+    grunt.registerTask('default', ['uglify', 'sass']);
 };
